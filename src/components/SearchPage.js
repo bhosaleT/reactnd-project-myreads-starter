9a5ab08b30cb8d9a63e06487d22cf39328beb745
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-28 10:36:16 +0530

    search component styled and the input is logging out to console

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class SearchPage extends React.Component {
-    render(){
-        return(
-            <div>
-            This is the search page.
-            </div>
-        )
-    }
+  searchBooks = query => {
+    console.log(`search for the book ${query}`);
+    /* TODO: 
+        1. use the booksApi to search for books 
+        2. Add that to a results array 
+        3. Add that array to state 
+        4. forward that state to Book Component.
+        */
+  };
+
+  render() {
+      
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">
+            &gt; Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={event => {
+                this.searchBooks(event.target.value);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchPage;
