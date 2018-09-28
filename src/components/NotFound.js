395465e86c5a814ae99aa9256f9906c8a19b4ac2
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-28 18:16:47 +0530

    updated readme

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => (
     <div>
-    <h3>Not Found</h3>
+        <iframe title="404" src="https://giphy.com/embed/xTiN0L7EW5trfOvEk0" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     </div>
 )
 
