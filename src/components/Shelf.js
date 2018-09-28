f9ebeb45a99d215ed5b81481ce9c7bde938888e4
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-28 13:30:43 +0530

    changed some CSS[fonts,colos] and added a loading screen component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Book from "./Book";
-
+import Loading from "./Loading";
 /* 
 -- Individual shelfs created for currentlyReading, wantToRead, Read and passing those props into it.
+-- Loading component is a simple component which creates a simple loading screen type animation.
 */
 
 const Shelf = props => (
@@ -11,21 +12,29 @@ const Shelf = props => (
       {/* Bookshelf for currentlyReading uses props.currentlyReading*/}
       <div className="bookshelf">
         <h2 className="bookshelf-title">Currently Reading</h2>
-        {props.currentlyReading.length > 0 && (
+        {props.currentlyReading.length > 0 ? (
           <Book shelvedBooks={props.currentlyReading} />
+        ) : (
+          <Loading />
         )}
       </div>
       {/* Bookshelf for wantToRead uses props.wantToRead*/}
       <div className="bookshelf">
         <h2 className="bookshelf-title">Want To Read</h2>
-        {props.wantToRead.length > 0 && (
+        {props.wantToRead.length > 0 ? (
           <Book shelvedBooks={props.wantToRead} />
+        ) : (
+          <Loading />
         )}
       </div>
       {/* Bookshelf for read uses props.read*/}
       <div className="bookshelf">
         <h2 className="bookshelf-title">Read</h2>
-        {props.read.length > 0 && <Book shelvedBooks={props.read} />}
+        {props.read.length > 0 ? (
+          <Book shelvedBooks={props.read} />
+        ) : (
+          <Loading />
+        )}
       </div>
     </div>
   </div>
