3a5341db9c96fe8c2a3ea1c2bdec709f389e92a9
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-29 12:54:30 +0530

    added a ratings system

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,63 +1,83 @@
 import React from "react";
-import ShelfChanger from './ShelfChanger';
-import PropTypes from 'prop-types';
+import ShelfChanger from "./ShelfChanger";
+import PropTypes from "prop-types";
+import StarRatingComponent from "react-star-rating-component";
 
 const Book = props => {
-
   /* Destructing the props object and getting the shelfedBooks from it.
   -- Was having trouble with books that dont have imageLinks so I just filtered those books out in this component
   -- Could have done in previous components, I just did it here. because i encountered the problem here.
   */
- 
+
   const { shelvedBooks, onChangeShelf } = props;
   const shelvedBooksWithThumbnails = shelvedBooks.filter(
     book => book.imageLinks
   );
-  return (
-    <div className="bookshelf-books">
+  return <div className="bookshelf-books">
       <ol className="books-grid">
         {/* 
          -- Loop over shelfedBooks and then show them   
          */
-        shelvedBooksWithThumbnails.length > 0 &&
-          shelvedBooksWithThumbnails.map(book => (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.thumbnail} )`
-                    }}
-                  />
-                  <ShelfChanger 
-                  book = {book}
-                  books = {shelvedBooksWithThumbnails}
-                  onChangeShelf={onChangeShelf}
-                  />
-                </div>
-                <div className="book-title">{book.title}</div>
-                {/* 
+        shelvedBooksWithThumbnails.length > 0 && shelvedBooksWithThumbnails.map(
+            book => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div
+                      className="book-cover"
+                      style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${book.imageLinks.thumbnail} )`
+                      }}
+                    />
+                    <ShelfChanger
+                      book={book}
+                      books={shelvedBooksWithThumbnails}
+                      onChangeShelf={onChangeShelf}
+                    />
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  {/* 
                     Checking for multiple authors by mapping over them 
                     */
-                book.authors &&
-                  book.authors.map((author, index) => (
-                    <div className="book-authors" key={index}>
-                      {author}
-                    </div>
-                  ))}
-              </div>
-            </li>
-          ))}
+                  book.authors &&
+                    book.authors.map((author, index) => (
+                      <div className="book-authors" key={index}>
+                        {author}
+                      </div>
+                    ))}
+                  <div className="book-rating">
+                    {book.averageRating !== undefined ? (
+                      <StarRatingComponent
+                        name="rate1"
+                        starCount={5}
+                        value={book.averageRating}
+                        editing={false}
+                        emptyStarColor={"#d3d3d3"}
+                        starColor={"#272727"}
+                      />
+                    ) : (
+                      <StarRatingComponent
+                        name="rate2"
+                        starCount={5}
+                        value={0}
+                        editing={false}
+                        emptyStarColor={"#d3d3d3"}
+                        starColor={"#272727"}
+                      />
+                    )}
+                  </div>
+                </div>
+              </li>
+            )
+          )}
       </ol>
-    </div>
-  );
+    </div>;
 };
 
 export default Book;
 Book.propTypes = {
   shelvedBooks: PropTypes.array.isRequired,
   onChangeShelf: PropTypes.func.isRequired
-}
+};
