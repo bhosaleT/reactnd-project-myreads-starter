a04f424fdfeacf9429afc5b37881ec40d6f05c91
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-29 19:53:31 +0530

    minor fixes after code review

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => (
-    <div>
-        <iframe title="404" src="https://giphy.com/embed/xTiN0L7EW5trfOvEk0" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-    </div>
-)
+  <div>
+    <iframe
+      title="404"
+      src="https://giphy.com/embed/xTiN0L7EW5trfOvEk0"
+      width="480"
+      height="480"
+      frameBorder="0"
+      className="giphy-embed"
+      allowFullScreen
+    />
+  </div>
+);
 
 export default NotFound;
