479174d8a035887e1dd45086c04ded793bf9a869
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-11-23 20:08:00 +0530

    updated css and animations

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Book from "./Book";
 import PropTypes from "prop-types";
-import Loading from "./Loading";
 /* SHELF COMPONENT */
 
 const Shelf = props => {
@@ -11,17 +10,9 @@ const Shelf = props => {
       <h2 className="bookshelf-title">{props.title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {books.length ? (
-            books.map((book, index) => (
-              <Book
-                book={book}
-                key={index}
-                onChangeShelf={props.onChangeShelf}
-              />
-            ))
-          ) : (
-            <Loading />
-          )}
+          {books.map((book, index) => (
+            <Book book={book} key={index} onChangeShelf={props.onChangeShelf} />
+          ))}
         </ol>
       </div>
     </div>
